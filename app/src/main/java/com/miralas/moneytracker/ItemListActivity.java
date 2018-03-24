9bae3f1c4b2b3aa2d1198709fa6f6ce680a1262a
Miralas <miralas777@yandex.ru>
2018-03-24 08:43:53 +0300

    Added everything from the webinar

---
+++ app/src/main/java/com/miralas/moneytracker/ItemListActivity.java
@@ -0,0 +1,90 @@
+package com.miralas.moneytracker;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tiburon on 23/03/2018.
+ */
+
+public class ItemListActivity extends AppCompatActivity {
+
+    private RecyclerView view;
+    private List<Item> data = new ArrayList<>();
+    private ItemListAdapter adapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+        createData();
+        view = findViewById(R.id.list);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ItemListAdapter();
+        view.setAdapter(adapter);
+//        adapter.notifyDataSetChanged();
+    }
+
+    private void createData() {
+        data.add(new Item("Молоко", 55));
+        data.add(new Item("Сыр", 300));
+        data.add(new Item("Хлеб", 50));
+        data.add(new Item("Шоколадка", 120));
+        data.add(new Item("Оплатил счет картой на корпоративе", 15467));
+        data.add(new Item("Автомобиль", 1250000));
+        data.add(new Item("CocaCola", 100));
+        data.add(new Item("", 0));
+        data.add(new Item("Холодильник", 32467));
+        data.add(new Item("Печенюшки", 100));
+        data.add(new Item("Новый SSD", 12000));
+        data.add(new Item("Уже не помню что тогда покупал", 5858));
+    }
+
+    private class ItemListAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+        @Override
+        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ItemViewHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_record, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(ItemViewHolder holder, int position) {
+            Item item = data.get(position);
+            holder.applyData(item);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+    }
+
+    private class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView title;
+        private final TextView price;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.item_title);
+            price = itemView.findViewById(R.id.item_price);
+        }
+
+        public void applyData(Item item) {
+            title.setText(item.getTitle());
+            price.setText(String.valueOf(item.getPrice()));
+        }
+    }
+}
